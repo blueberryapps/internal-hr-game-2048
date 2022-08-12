6864ed5a2ca7138e305c5c4501017d9b0805d077
Jan Hudak <janhudak16@gmail.com>
2022-08-12 09:00:14 +0200

    removed pages game and hall-of-fame

--- pages/hall-of-fame.tsx
+++
@@ -1,8 +0,0 @@
-import { Container } from "@mui/material";
-import { NextPage } from "next";
-import * as React from "react";
-import HallOfFame from "../components/HallOfFame";
-
-const HallOfFamePage: NextPage = () => <HallOfFame />;
-
-export default HallOfFamePage;
